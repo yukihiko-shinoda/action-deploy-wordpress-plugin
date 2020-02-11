fb655edcd5528d9f4cf0d2debe4d77d215831903
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2020-02-11 15:29:28 +0900

    Fix bug of Shell Script syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -eu
 export GIT_PATH_CHECKED_OUT="${GITHUB_WORKSPACE}"
 export DEPLOY_VERSION="${GITHUB_REF#refs/tags/}"
 if [ "${DEPLOY_VERSION}" = "${EXPECT_DEPLOY_VERSION_FOR_TEST}" ]; then
-  export DEPLOY_VERSION = "${DEPLOY_VERSION_FOR_TEST}"
+  export DEPLOY_VERSION="${DEPLOY_VERSION_FOR_TEST}"
 fi
 exec entrypoint-deploy-wordpress-plugin "${@}"
